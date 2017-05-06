75abc7b6b592f07d8a7c2e0112df376752128b74
vamsiampolu <vamsiampolu3000@gmail.com>
2017-05-07 04:33:43 +1000

    WellGrid with styled components

--- src/components/WellGrid.js
+++ src/components/WellGrid.js
@@ -3,55 +3,74 @@ import PropTypes from 'prop-types'
 import SquareBlock from './SquareBlock'
 import styled from 'styled-components'
 
-const { number, arrayOf, array } = PropTypes
+const {number, arrayOf, array} = PropTypes
 
-const UL = styled.ul`
+const LI = styled.li`
+  position: absolute;
+  transition: top 0.1s linear;
+  width: ${props => `${props.widthPercent}%`};
+  height: ${props => `${props.heightPercent}%`};
+  top: ${props => `${props.row * props.heightPercent}%`};
+  left: ${props => `${props.col * props.widthPercent}%`};
+`
+
+function GridListItem (props) {
+  const {widthPercent, heightPercent, row, col, blockValue} = props
+  return (
+    <LI
+      widthPercent={widthPercent}
+      heightPercent={heightPercent}
+      row={row}
+      col={col}
+    >
+      <SquareBlock color={blockValue[1]} />
+    </LI>
+  )
+}
+
+GridListItem.propTypes = {
+  widthPercent: number,
+  heightPercent: number,
+  row: number,
+  col: number,
+  blockValue: array
+}
+
+const WellGridList = styled.ul`
   margin: 0;
   padding: 0;
   list-style-type: none;
 `
 
-const LI = styled.li`
-  width: 25%;
-  height: 25%;
-  width: ${props => (props.widthPercent + '%')};
-  height: ${props => (props.heightPercent + '%')};
-  top: ${props => ((props.row * props.heightPercent) + '%')};
-  left: ${props => ((props.row * props.widthPercent) + '%')};
-`
-
-LI.propTypes = {
-  widthPercent: number,
-  heightPercent: number,
-  row: number
-}
-
 export default class WellGrid extends React.Component {
-
-  shouldComponentUpdate (nextProps) {
-    const { props } = this
-    return nextProps.grid !== props.grid
-  }
+  // TODO add shouldComponentUpdate?
 
   renderGridBlocks () {
-    debugger
-    const { grid } = this.props
-    const blocks = []
+    const {props} = this
+    const {grid} = props
+
+    let blocks = []
+
     const rows = grid.length
     const cols = grid[0].length
     const widthPercent = 100 / cols
-    const heightPercent = 100 / rows
-    let blockValue
+    const heightPercent = 100 / cols
 
     for (let row = 0; row < rows; row++) {
-      for (let col = 0; col > cols; col++) {
+      for (let col = 0; col < cols; col++) {
         if (grid[row][col]) {
-          blockValue = grid[row][col]
-          console.log('COL', col, 'ROW', row)
-          console.log('Block Value', grid[row][col])
-          blocks.push(<LI key={blockValue[0]} row={row} heightPercent={heightPercent} widthPercent={widthPercent}>
-            <SquareBlock color={blockValue[1]} />
-          </LI>)
+          let blockValue = grid[row][col]
+          // TODO extract to seperate component
+          blocks.push(
+            <GridListItem
+              widthPercent={widthPercent}
+              heightPercent={heightPercent}
+              row={row}
+              col={col}
+              blockValue={blockValue}
+              key={blockValue[0]}
+            />
+          )
         }
       }
     }
@@ -59,10 +78,6 @@ export default class WellGrid extends React.Component {
   }
 
   render () {
-    return (<UL>{this.renderGridBlocks()}</UL>)
+    return <WellGridList>{this.renderGridBlocks()}</WellGridList>
   }
-}
-
-WellGrid.propTypes = {
-  grid: arrayOf(arrayOf(array)).isRequired
 }
